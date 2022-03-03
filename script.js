479379dae0069b20d32061b70aed5b7905da44c8
Carrington <hartis.c99@gmail.com>
2022-03-02 22:32:27 -0500

    quiz has its core functions, added style

--- script.js
+++ script.js
@@ -26,8 +26,8 @@ var triviaQuestions = [
   },
   {
     id: 3,
-    next: null,
     question: "Which animal can stand on its tail?",
+    next: null,
     answers: {
       a: "Monkeys",
       b: "Cats",
@@ -43,11 +43,15 @@ var currentQuestion = triviaQuestions[0];
 var correctChoice = currentQuestion.correctAnswer;
 var questionElement = document.getElementById("question");
 var answerElement = document.querySelectorAll("[data-choice]");
-console.log(answerElement);
 var currentAnswers = document.getElementById("answers-container");
+var endQuiz = document.getElementById("endQuiz");
+var initials = document.getElementById("inputElement").value;
+var submitBtn = document.getElementById("submitBtn");
+var sec = 30;
+var timer;
+
 questionElement.innerText = currentQuestion.question;
 answerElement.innerText = currentQuestion.answers;
-var i = 0;
 
 // hide questions.
 function hideQuestion() {
@@ -56,9 +60,20 @@ function hideQuestion() {
 }
 hideQuestion();
 
+function startTime() {
+  timer = setInterval(function () {
+    document.getElementById("timerDisplay").innerHTML = "00:" + sec;
+    sec--;
+    if (sec < 0) {
+      clearInterval(timer);
+    }
+  }, 1000);
+}
+
 // When the start button is clicked, a timer begins and a question appears.
-var begin = beginQuiz.addEventListener("click", function (event) {
+beginQuiz.addEventListener("click", function (event) {
   event.preventDefault();
+  startTime();
   // a question and its answers appear, the start button disappears.
   if (
     questionElement.style.display === "none" &&
@@ -77,6 +92,7 @@ function showQuestion(question) {
     var answerElement = document.getElementById(choice);
     answerElement.innerText = question.answers[choice];
     answerElement.addEventListener("click", handleAnswerClick);
+    console.log(answerElement);
   }
 }
 showQuestion(currentQuestion);
@@ -85,14 +101,23 @@ function handleAnswerClick(event) {
   event.preventDefault();
   console.log(event.target.dataset.choice);
 
-  // When an answer choice is clicked, it turns green for correct and red for incorrect.
+  // When an answer choice is clicked, it adds time if the answer is correct.
   if (event.target.dataset.choice === correctChoice) {
-    console.log("correct");
     console.log(currentQuestion);
+    sec += 10;
+    document.getElementById("timerDisplay").innerHTML = "00:" + sec;
   } else {
-    console.log("wrong");
+    // If it is incorrect, time from the timer is subtracted.
+    sec -= 10;
+    document.getElementById("timerDisplay").innerHTML = "00:" + sec;
   }
-  newQuestion();
+
+  if (currentQuestion.next) {
+    newQuestion();
+  } else {
+    showEndScreen();
+    hideQuestion();
+  }
 }
 
 // The next question and answer choices are shown.
@@ -101,12 +126,28 @@ function newQuestion() {
     (questionItem) => currentQuestion.next === questionItem.id
   );
   currentQuestion = nextQuestion[0];
-
+  correctChoice = currentQuestion.correctAnswer;
   showQuestion(currentQuestion);
 }
 
-// If it is incorrect, time from the timer is subtracted.
+function hideEndScreen() {
+  endQuiz.style.display = "none";
+}
+hideEndScreen();
 
 // When all the questions are answered or the timer reaches 0, user is given a score based off the time they have left.
+function showEndScreen() {
+  endQuiz.style.display = "block";
+  clearInterval(timer);
+}
 
 // User is asked for initials and their score is stored in local storage.
+submitBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+  var initialsValue = document.getElementById("inputElement").value;
+  console.log(initialsValue);
+  localStorage.setItem(
+    "Initials",
+    initialsValue.toUpperCase() + " - Score: " + sec
+  );
+});
